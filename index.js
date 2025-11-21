021f6b0e826bfc6c39e076d843a35b22fb6c7220
dheim123 <dheim@midland.k12.ia.us>
2025-11-21 12:09:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function markAsDone(todos) {
+  let i = 0;
   
-  while (i < todos.length) {
+  while (i < todos.length; i++) {
     todos[i].description =  todos[i].description + 'done - ';
-
-    i++;
   }
   return todos;
 }
+
 
 
 
